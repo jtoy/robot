bb27872dc7eabb501c0bb7366f7f0c9731825184
rubychilds <ruby.childs1@gmail.com>
2016-12-04 14:50:41 -0500

    working on single robot script

--- image.py
+++ image.py
@@ -1,41 +1,33 @@
-
-
 import subprocess
+
 
 def do(cmd):
 	print(cmd)
-	p =subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	for line in p.stdout.readlines():
 		print line
-
 	retval = p.wait()
 
 
 def classify(n):
 	suffix = str(n)
-	imageFile = 'images/img'+suffix+'.jpg'
-	dataFile = 'images/data'+suffix
+	imageFile = 'images/img' + suffix + '.jpg'
+	dataFile = 'images/data' + suffix
 	latestImage = 'images/latest_img.jpg'
 	latestData = 'images/latest_data'
-        do('echo "I\'m thinking." | flite')
-	do('cp /dev/shm/mjpeg/cam.jpg '+imageFile);
-	do('ln -f '+imageFile+' '+latestImage);
-	do('bash run_and_parse_inception.sh '+imageFile+ " " +dataFile)
-
-	do('ln -f '+dataFile+' '+latestData);
-
-        do('{ echo "I think I see a "; cat '+dataFile+' |  sed -e \'$ ! s/$/. or maybe a/\'; } | flite')
-
-	do('echo '+suffix+' > images/INDEX')
+	do('echo "I\'m thinking." | flite')
+	do('cp /dev/shm/mjpeg/cam.jpg ' + imageFile)
+	do('ln -f ' + imageFile + ' ' + latestImage)
+	do('bash run_and_parse_inception.sh ' + imageFile + " " + dataFile)
+	do('ln -f ' + dataFile + ' ' + latestData)
+	do('{ echo "I think I see a "; cat ' + dataFile + ' |  sed -e \'$ ! s/$/. or maybe a/\'; } | flite')
+	do('echo ' + suffix + ' > images/INDEX')
 
 
-i=0
-read_data = 0
-
-with open('images/INDEX', 'r') as f:
-	read_data = int(f.read())
-
-i += read_data + 1;
-
-
-classify(i)
+def classify_image():
+	i = 0
+	read_data = 0
+	with open('images/INDEX', 'r') as f:
+		read_data = int(f.read())
+	i += read_data + 1
+	classify(i)
---
+++ main.py
@@ -0,0 +1,63 @@
+from image import classify
+from drive_safe import gof, stop, left, right, backward,
+
+
+stopped = True
+turning = False
+THRESH = 25
+turnCount = 0
+maxTurnCount = 3
+
+
+def main():
+    while(True):
+        print("driving!")
+
+        mind = 1000
+        d = []
+
+        for i in range(3):
+            d.append(distance(i))
+            if d[i] < mind:
+                mind = d[i]
+            print(d[i])
+
+        print(d)
+        print("Min d " + str(mind))
+
+        if (mind < 6 and stopped and turning):
+            do('echo "backing up." | flite ')
+            backward(255, 1)
+        elif (turnCount > maxTurnCount):
+            do('echo "backing up." | flite ')
+            backward(255, 1)
+            turnCount = 0
+        elif (mind >= THRESH and stopped):
+            stopped = False
+            turning = False
+            gof()
+            turnCount = 0
+        elif (mind < THRESH and not stopped):
+            stopped = True
+
+        elif (mind < THRESH and stopped):
+            if (not turning):
+                stop()
+                turning = True
+            else:
+                turnCount++
+
+            if d[2] > d[0]:
+                do('echo "turning left." | flite ')
+                left(255, 0.5)
+            else:
+                do('echo "turning right." | flite ')
+                right(255, 0.5)
+
+            time.sleep(0.3)
+            stopped = True
+            classify_image()
+
+
+if __name__ == '__main__':
+    main()
