85478355e39d7b6a0a55f2f108ee1f35df22370c
rubychilds <ruby.childs1@gmail.com>
2016-12-04 18:28:26 -0500

    distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from image import classify
-from drive_safe import gof, stop, left, right, backward, turnOffMotors
+from drive_safe import gof, stop, left, right, backward, turnOffMotors, distance
 
 
 stopped = True
